1f5f34fc2c2ad1be6092ee9403a7a37490da82b4
Łukasz Topolski <l.topolski@stud.elka.pw.edu.pl>
2023-02-12 23:05:07 +0100

    insert data file rename

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 if __name__ == "__main__":
 
-    for file in ('remove_tables.sql', 'create_tables.sql', 'dane.sql'):
+    for file in ('remove_tables.sql', 'create_tables.sql', 'data.sql'):
         conn = get_connection(config())
         commands = get_commands(os.path.join('./sql_scripts', file))
         exec_commands(commands, conn, False)
